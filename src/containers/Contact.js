47bb282fac5ce0472fdfd5eacd902fe1484b2d4e
lfaivre <lfaivre@asu.edu>
2019-08-20 23:00:08 -0700

    just need to edit back button

--- src/containers/Contact.js
+++ src/containers/Contact.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../styles/Contact.css";
+import BackButton from "../components/BackButton";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
@@ -11,12 +12,30 @@ class Contact extends React.Component {
   render() {
     return (
       <div className="contact col-12 col-lg-6">
-        <div>
-          <FontAwesomeIcon icon={faGithub} />
-          <FontAwesomeIcon icon={faLinkedin} />
-          <FontAwesomeIcon icon={faPaperPlane} />
+        <div className="contact-links">
+          <a
+            href="https://github.com/lfaivre"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faGithub} size="6x" />
+          </a>
+          <a
+            href="https://www.linkedin.com/in/lorenzofaivre/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faLinkedin} size="6x" />
+          </a>
+          <a
+            href="mailto:lorenzofaivre@gmail.com"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <FontAwesomeIcon icon={faPaperPlane} size="6x" />
+          </a>
         </div>
-        <button onClick={() => this.handleClick("landing")}>back</button>
+        <BackButton handleClick={this.handleClick} />
       </div>
     );
   }
